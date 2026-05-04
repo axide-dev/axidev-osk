ef1b5ca8ccefaf5fcfa5b3bd27ae34fcf24d5e2a
Zied Yousfi <zied.essaber@gmail.com>
2026-05-04 21:47:46 +0200

    fix(release): bump to 0.14.0

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
 
           axidev-osk = python.pkgs.buildPythonApplication {
             pname = "axidev-osk";
-            version = "0.13.7";
+            version = "0.14.0";
             pyproject = true;
             src = self;
 
